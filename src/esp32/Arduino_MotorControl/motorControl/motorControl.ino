4868d5637092927967115d8c406457ffe91dc8b4
PheonixBoi <116541930+PheonixBoi@users.noreply.github.com>
2026-08-11 00:31:36 +0200

    Procedures and serial read
    -Added serial read functionality for controlling speed and steering of bot
    
    -Added procedures for different states
    
    -Added full procedure for LeavingParking
    
    TODO:
    -check conditions required for driving straight
    -check procedure for driving around t.lights in both py code and arduino code

--- src/esp32/Arduino_MotorControl/motorControl/motorControl.ino
+++ src/esp32/Arduino_MotorControl/motorControl/motorControl.ino
@@ -1,6 +1,17 @@
 #include <AccelStepper.h>
 
 int numOfSteps = 0;
+String Placeholder = "Stuff: ";
+String data;
+float serialDir, serialSpd;
+
+bool isTurningCorner = false; 
+bool firstInstanceTurning = true;
+
+int maxDrivingSpeed = 2000;
+
+int cornersTurned = 0;
+
 #define STEERING_STEP_PIN 32
 #define STEERING_DIRECTION_PIN 33
 
@@ -26,6 +37,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
 
+
   pinMode(F_TRIG_PIN, OUTPUT);
   pinMode(F_ECHO_PIN, INPUT);
 
@@ -35,11 +47,11 @@ void setup() {
   pinMode(R_TRIG_PIN, OUTPUT);
   pinMode(R_ECHO_PIN, INPUT);
 
-  drivingStepper.setMaxSpeed(1000);
+  drivingStepper.setMaxSpeed(maxDrivingSpeed);
   // Set the speed in steps per second:
-  drivingStepper.setSpeed(1000);
+  drivingStepper.setSpeed(maxDrivingSpeed);
   // Step the motor with a constant speed as set by setSpeed():
-  drivingStepper.setAcceleration(100);
+  drivingStepper.setAcceleration(250);
   drivingStepper.move(100);
 
   steeringStepper.setMaxSpeed(1000);
@@ -48,31 +60,47 @@ void setup() {
   // Step the motor with a constant speed as set by setSpeed():
   steeringStepper.setAcceleration(100);
 
+
 }
 
 void loop() {
-  //Leaving Parking Zone
-  if(getDistance(F_TRIG_PIN, F_ECHO_PIN) <= 5 && (getDistance(L_TRIG_PIN, L_ECHO_PIN) <= 5 || getDistance(R_TRIG_PIN, R_ECHO_PIN) <= 5)) //&&magenta
+  if (Serial.available() > 0)
   {
-    Serial.println("trying to leave parking");
+    serialDir = Serial.parseFloat();
+    serialSpd =  Serial.parseFloat();
   }
+
+  data = Placeholder + serialDir + " " + serialSpd;
+  Serial.println(data);
+
+  drivingStepper.setSpeed(maxDrivingSpeed * serialSpd);
+
+  drivingStepper.run();
+  steeringStepper.run();
+
 
   //Driving Straight
-  if(getDistance(F_TRIG_PIN, F_ECHO_PIN) >= 30 && (getDistance(L_TRIG_PIN, L_ECHO_PIN) <= 30 || getDistance(R_TRIG_PIN, R_ECHO_PIN) <= 30)) 
-  {
-    Serial.println("Driving straight");
+  if(getDistance(F_TRIG_PIN, F_ECHO_PIN) >= 10 && serialDir < 0.05f && serialDir > -0.05f)
+  { //might remove get distance from this conditional
+    DriveStraight();
   }
 
-  //Turning Corner
-  if(getDistance(F_TRIG_PIN, F_ECHO_PIN) < 30 && (getDistance(L_TRIG_PIN, L_ECHO_PIN) >= 30 || getDistance(R_TRIG_PIN, R_ECHO_PIN) >= 30)) //&&colour = blue || orange
+  //TurningCorner
+  else if (isTurningCorner || (serialDir >= 0.9f && getDistance(R_TRIG_PIN, R_ECHO_PIN) > 30) || (serialDir <= -0.9f && getDistance(L_TRIG_PIN, L_ECHO_PIN) > 30) )
   {
-    Serial.println("Turning corner");
+    TurnCorner();
   }
 
   //Passing t.lights
-  if(getDistance(F_TRIG_PIN, F_ECHO_PIN) < 10) //&&colour = "red" || "green"
+  else if (serialDir != 0)
   {
-    Serial.println("Passing t.light");
+    SteeringAroundTrafficLight();
+  }
+
+    //Leaving Parking Zone
+  else if (cornersTurned = 0 && getDistance(F_TRIG_PIN, F_ECHO_PIN) <= 5 && (getDistance(L_TRIG_PIN, L_ECHO_PIN) <= 5 || getDistance(R_TRIG_PIN, R_ECHO_PIN) <= 5)) //&&magenta
+  {
+    LeaveParking();
   }
 }
 
@@ -83,5 +111,69 @@ float getDistance(int trigPin, int echoPin)
   digitalWrite(trigPin, LOW);
 
   int duration = pulseIn(echoPin, HIGH);
-  return duration/58;
+  return duration / 58;
+}
+
+void DriveStraight()
+{
+  Serial.println("Driving straight");
+  steeringStepper.moveTo(0);
+}
+
+void TurnCorner()
+{
+  if(firstInstanceTurning)
+  {
+    isTurningCorner = true;
+    firstInstanceTurning = false;
+  }
+
+  else if(!firstInstanceTurning)//some condition that must be met in regards to how many wheel steps it took while steering
+  {
+    isTurningCorner = false;
+    firstInstanceTurning = true;
+    cornersTurned++;
+  }
+
+  Serial.println("Turning corner");
+  steeringStepper.moveTo(25 * serialDir); //25 steps represents 45 degrees; multiplying by serialDir (which is -1.o to 1.0), determines how much of 45 degrees the wheels must turn
+}
+
+void SteeringAroundTrafficLight()
+{
+  Serial.println("Passing t.light");
+  steeringStepper.moveTo(25 * serialDir);
+}
+
+void LeaveParking()
+{
+  Serial.println("trying to leave parking");
+  if(getDistance(L_TRIG_PIN, L_ECHO_PIN) <= 5)
+  {
+    steeringStepper.moveTo(25);
+    steeringStepper.runToPosition();
+
+    drivingStepper.moveTo(400);
+    drivingStepper.runToPosition();
+
+    steeringStepper.moveTo(0);
+    steeringStepper.runToPosition();
+
+    drivingStepper.moveTo(800);
+    drivingStepper.runToPosition();
+  }
+  else if(getDistance(R_TRIG_PIN, R_ECHO_PIN) <= 5)
+  {
+    steeringStepper.moveTo(-25);
+    steeringStepper.runToPosition();
+
+    drivingStepper.moveTo(400);
+    drivingStepper.runToPosition();
+
+    steeringStepper.moveTo(0);
+    steeringStepper.runToPosition();
+
+    drivingStepper.moveTo(800);
+    drivingStepper.runToPosition();
+  }
 }
